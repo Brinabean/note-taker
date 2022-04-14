d8f97aab7b51bc032525f455e2bd3d7fff7ce753
Brina <sabrinazanin97@gmail.com>
2022-04-14 19:42:51 -0400

    finish

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
 const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/htmlRoutes");
 
