052777dc8849ed1ab43fe44332f89191cebb50b2
Brina <sabrinazanin97@gmail.com>
2022-05-04 20:46:18 -0400

    heroku fix

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
-
-const apiRoutes = require("./routes/api");
-const htmlRoutes = require("./routes/htmlRoutes");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming JSON data
-app.use(express.json());
-// parse incoming string or array data
+const apiRoutes = require("./routes/api");
+const htmlRoutes = require("./routes/htmlroutes");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
+
+// Use apiRoutes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
